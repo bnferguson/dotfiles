cf8eb87872575ed65bfe4852a0f2ab41872b20d8
Brandon Ferguson <bnferguson@gmail.com>
2018-10-02 21:42:32 +0200

    Fix gco completion

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -16,9 +16,9 @@ alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
 alias gc='git commit'
 alias gca='git commit -a'
-compdef _git gco='git-checkout'
+alias gco='git checkout'
+compdef _git gco=git-checkout
 alias gcb='git copy-branch-name'
 alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gac='git add -A && git commit -m'
+alias gst='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias ge='git-edit-new'
